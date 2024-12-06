afe125bc678d892764b39f2c01afd1996523244d
Gabriel Paiao <costapaiao@hotmail.com>
2024-12-06 14:11:19 -0300

    Initial commit :tada:

---
+++ src/main/java/com/example/studyingSpringSecurity/StudyingSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyingSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyingSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyingSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studyingSpringSecurity/StudyingSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyingSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
